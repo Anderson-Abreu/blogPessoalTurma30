900491de614b555feb7ff9d7600576b7af87c000
Anderson-Abreu <prof.andersonmabreu@gmail.com>
2021-10-07 07:51:54 -0300

    put-delele postagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
@@ -6,6 +7,7 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 
 const routes: Routes = [
   {path:'cadastrar', component: CadastrarComponent},
@@ -14,7 +16,9 @@ const routes: Routes = [
   {path:'inicio', component: InicioComponent},
   {path:'tema', component: TemaComponent},
   {path:'tema-edit/:id', component: TemaEditComponent},
-  {path:'tema-delete/:id', component: TemaDeleteComponent} 
+  {path:'tema-delete/:id', component: TemaDeleteComponent},
+  {path:'postagem-edit/:id', component: PostagemEditComponent},
+  {path:'postagem-delete/:id', component: PostagemDeleteComponent} 
 ];
 
 @NgModule({
--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -1,9 +1,8 @@
-import { Postagem } from './../model/Postagem';
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment.prod';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.prod'
-
+import { Postagem } from '../model/Postagem';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +11,7 @@ export class PostagemService {
 
   constructor(private http: HttpClient) { }
 
-  token = { 
+  token = {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
@@ -20,7 +19,21 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://backendblogpessoal.herokuapp.com/postagens', this.token)
   }
 
-  postPostagem(postagem: Postagem): Observable<Postagem>{
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://backendblogpessoal.herokuapp.com/postagens/${id}`, this.token)
+  }
+
+  postPostagem(postagem: Postagem) : Observable<Postagem>{
     return this.http.post<Postagem>('https://backendblogpessoal.herokuapp.com/postagens', postagem, this.token)
   }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://backendblogpessoal.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`https://backendblogpessoal.herokuapp.com/postagens/${id}`, this.token)
+  }
+
+
 }
